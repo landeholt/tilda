3024a76958b2b1b97863517dff8e0d0472c027db
landeholt <johnlan@kth.se>
2020-12-26 12:38:41 +0100

    changed to timeit

--- help-code/timing.py
+++ help-code/timing.py
@@ -1,39 +1,36 @@
 # Author: John Landeholt [TA]
-from time import time
-import concurrent.futures
+from itertools import repeat
 import random
+import timeit
+
+REPEAT = 100
+NUMBER = 100
 
 def timing(f):
-    """
-        Usage:  @timing
-                def your_function(args):
-                    return 'YOUR RETURN'
-    """
+    
+    def avg(coll):
+        return sum(coll) / len(coll)
+    def best_of(coll, n = 3):
+        sorted_coll = sorted(coll)
+        return sorted_coll[:n]
     def wrap(*args, **kw):
-        times = 10000
-        ts = time()
-        future_list = []
-        res = None
-        with concurrent.futures.ThreadPoolExecutor(max_workers = 13) as executor:
-            for _ in range(times):
-                future_list.append(executor.submit(f, *args))
-            for fut in future_list:
-                res = fut.result()
+        def executor():
+            return f(*args)
+        t = timeit.repeat(executor, repeat= REPEAT, number= NUMBER)
+        avg_t = avg(t)
+        best_of_t = avg(best_of(t))
+        print(f'function {f.__name__} executed in: {round(avg_t / 1e-3, 3)}ms best average: {round(best_of_t / 1e-3, 3)}ms')
 
-        te = time()
-        avg_time = ((te - ts) / times) / 1e-3
-        print(f'function {f.__name__} took: {round(avg_time,3)}ms')
-        return res
-
+        return executor()
     return wrap
 
-@timing
-def linear_search(l, s):
-    for x in l:
-        if x == s: return x
-    return None
-
 if __name__ == '__main__':
+    @timing
+    def linear_search(l, s):    
+        for x in l:
+           if x == s: return x
+        return None
     l = list(range(100000))
     random.shuffle(l)
-    linear_search(l,42)
+    n = linear_search(l,42)
+    print(n)
